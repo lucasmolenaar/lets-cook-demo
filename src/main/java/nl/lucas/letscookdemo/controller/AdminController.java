5e75b96028c2aace5e0108beab8c3fecec9696fc
lucasmolenaar <lucasmolenaar@hotmail.com>
2021-05-29 16:50:40 +0200

    added AdminController

---
+++ src/main/java/nl/lucas/letscookdemo/controller/AdminController.java
@@ -0,0 +1,15 @@
+package nl.lucas.letscookdemo.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AdminController {
+
+    @RequestMapping(value = "/admin")
+    public ResponseEntity<Object> getMessage() {
+        return new ResponseEntity<>("SECURED REST endpoint: /admin", HttpStatus.OK);
+    }
+}
